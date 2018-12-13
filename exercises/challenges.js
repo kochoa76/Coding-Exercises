dc8fc87f59bdaf9c52a032338c948d60bab12d75
github username <github email address>
2018-12-13 11:00:15 -0600

    add recursion problems

--- exercises/challenges.js
+++ exercises/challenges.js
@@ -77,33 +77,7 @@ return dup
     return !str.match(/([a-z]).*\1/i);
   }
 
-  //insertion sort
-  // Big O = n^2 
-
-  function findMinAndRemove(array){
-      let min = array[0];
-      let minIndex = 0;
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] < min){
-          min = array[i];
-          minIndex = i;
-        }
-    }
-      array.splice(minIndex, 1)
-      return min
-}
-
-function insertionSort(array){
-    let newMin;
-    let sorted = []
-    while( array != 0) {
-      let newMin = findMinAndRemove(array)
-      sorted.push(newMin)
-    }
-    return sorted;
-}
-
-
+  
   // Merge Sort Algorithm:
   // Big O = n log(n)
 
