fc922a419939d53eedfc72825e2f91c3da0ace00
github username <github email address>
2018-12-13 10:55:00 -0600

    add recursion problems

--- exercises/challenges.js
+++ exercises/challenges.js
@@ -76,3 +76,102 @@ return dup
   function isIsogram(str){
     return !str.match(/([a-z]).*\1/i);
   }
+
+  // Merge Sort Algorithm:
+
+  function findMinAndRemoveSorted(array){
+    return array.shift()
+  }
+
+  function merge(firstHalf, secondHalf){
+      let sorted = []
+
+
+      while(firstHalf.length !=0 && secondHalf.length != 0) {
+        if(firstHalf[0] < secondHalf[0]) {
+          let currentMin = findMinAndRemoveSorted(firstHalf)
+          sorted.push(currentMin)
+      } else {
+          let currentMin = findMinAndRemoveSorted(secondHalf)
+          sorted.push(currentMin)
+      }
+    }
+      return sorted.concat(firstHalf).concat(secondHalf)
+
+  }
+
+  function mergeSort(array) {
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    let sorted;
+
+    if(array.length < 2){
+      return array
+    } else {
+     sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+    return sorted;
+  }
+
+  // Recursion problems:
+
+  function printString(word){
+    console.log(word[0]);
+
+    if(word.length > 1){
+      let subword = word.substring(1, word.length)
+      printString(subword)
+  } else {
+    return true;
+  }
+}
+
+
+  function reverseString(word){
+
+    if (word.length < 2) {
+      return word;
+    } else {
+      return reverseString(word.substring(1)) + word[0]
+    }
+
+  }
+
+  function isPalindrome(sentence){
+
+    if (sentence.length < 2) {
+      return true;
+    } else if( sentence[sentence.length -1] === sentence[0]) {
+        return isPalindrome(sentence.substring(1, sentence.length - 1))
+    } else {
+      return false;
+    }
+  }
+
+
+  function addUpTo(array, index) {
+    if (index) {
+  	return array[index] + addUpTo(array, --index)
+  }else {
+  	return array[index]
+  }
+}
+
+function maxOf(myArray) {
+  if (myArray.length === 1) {
+    return myArray[0];
+  } else {
+    return Math.max(myArray.pop(), maxOf(myArray));
+  }
+}
+
+function includesNumber(myArray, myNumber) {
+  if (!myArray.length) {
+    return false;
+  } else if (myArray[0] === myNumber) {
+    return true;
+  } else {
+    return includesNumber(myArray.slice(1), myNumber);
+  }
+}
