83d35fd8b3baed55a25cc6f4e1c9e27c0c3b4073
github username <github email address>
2018-12-14 11:59:13 -0600

    add linked list exercises

--- exercises/challenges.js
+++ exercises/challenges.js
@@ -203,3 +203,75 @@ function includesNumber(myArray, myNumber) {
     return includesNumber(myArray.slice(1), myNumber);
   }
 }
+
+let firstNode = {name: 'susie', next: 'rkjasj'}
+let secondNode = {name: 'sam', next: 'asnan'}
+let lastNode = {name: 'charlie', next: null}
+let linkedList = 'whana'
+let collection = {rkjasj: secondNode, asnan: lastNode, whana: firstNode}
+
+  function getName(node) {
+      return node.name
+  }
+
+  function headNode(linkedList, collection) {
+    return collection[linkedList]
+  }
+
+  function next(node, collection) {
+    let nextNode = node.next
+
+    return collection[`${nextNode}`]
+  }
+
+  function nodeAt(index, linkedList, collection) {
+    let currentNode = collection[linkedList]
+      for(let i=0; i < index; i++) {
+        currentNode = next(currentNode, collection)
+      }
+    return currentNode
+  }
+
+  function addressAt(index, linkedList, collection) {
+      if(index === 0) {
+        return linkedList
+      } else {
+        return nodeAt(index-1, linkedList, collection).next
+      }
+
+  }
+
+  function indexAt(node, collection, linkedList) {
+        let currentIndex =0;
+        let currentNode = headNode(linkedList, collection)
+
+        while (currentNode != node) {
+            currentIndex++
+            currentNode = next(currentNode, collection)
+        }
+        return currentIndex++
+  }
+
+  function insertNodeAt(index, newAddress, linkedList, collection) {
+        let previousNode = nodeAt(index -1 , linkedList, collection)
+        let subsequentNode = nodeAt(index, linkedList, collection)
+
+        let previousNodeIdx = indexAt(previousNode, collection, linkedList)
+        let subsequentNodeIdx = indexAt(subsequentNode, collection, linkedList)
+
+        let previousNodeAddress= addressAt(previousNodeIdx, linkedList, collection)
+        let subsequentNodeAddress = addressAt(subsequentNodeIdx, linkedList, collection)
+        previousNode.next = newAddress
+        let newNode = collection[newAddress]
+        newNode.next = subsequentNodeAddress
+  }
+
+  function deleteNodeAt(index, linkedList, collection) {
+      let previousNode;
+      let currentNode = headNode(linkedList, collection)
+      for( let i =0; i < index; i++){
+        previousNode = currentNode
+        currentNode = next(currentNode, collection)
+      }
+      previousNode.next = currentNode.next
+  }
