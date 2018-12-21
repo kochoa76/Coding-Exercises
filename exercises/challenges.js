eaa462e6348cb7bd74444a5c7d3b9b3af6ec2aad
github username <github email address>
2018-12-21 11:22:22 -0600

    add new exericse on codewars

--- exercises/challenges.js
+++ exercises/challenges.js
@@ -337,3 +337,71 @@ function validParentheses(parens){
   //then replace it with '' so that if at the end the string is == '' then the response will be true.
   //if the response has a lagging '(' that means it does not == '' and thus the string is false.
 }
+
+function validParentheses(parens){
+  var n = 0;
+  for (var i = 0; i < parens.length; i++) {
+    if (parens[i] == '(') n++;
+    if (parens[i] == ')') n--;
+    if (n < 0) return false;
+  }
+
+  return n == 0;
+}
+
+// Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+//
+// ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+// Test.assertEquals(iqTest("2 4 7 8 10"),3);
+// Test.assertEquals(iqTest("1 2 2"), 1);
+
+
+function checkEvenOdd(numbers){
+	 let arrayOfNumbers = numbers.split(' ')
+    let numArray = arrayOfNumbers.filter(num => num.trim() != '')
+	let integerArr = numArray.map(num => parseInt(num))
+
+	let evenCount =0
+	let oddCount =0
+	for (let i=0; i< integerArr.length; i++){
+		if (integerArr[i]%2 ==0){
+			evenCount += 1
+        } else if (integerArr[i]%2 ===1){
+			oddCount += 1
+        }
+    }
+	if(evenCount>oddCount){
+		return 'even'
+    } else {
+		return 'odd'
+    }
+}
+
+function iqTest(numbers){
+    let arrayOfNumbers = numbers.split(' ')
+    let numArray = arrayOfNumbers.filter(num => num.trim() != '')
+	let integerArr = numArray.map(num => parseInt(num))
+
+
+  if (checkEvenOdd(numbers) === 'even'){
+	  return numIndex= integerArr.findIndex(function(num){
+		  return !(num%2 ===0)
+      }) + 1
+  } else if (checkEvenOdd(numbers) === 'odd'){
+    return numIndex = integerArr.findIndex(function(num){
+      return !(num%2 === 1)
+    }) + 1
+
+  }
+}
+
+//alternate answers
+
+function iqTest(numbers){
+  numbers = numbers.split(" ").map(function(el){return parseInt(el)});
+
+  var odd = numbers.filter(function(el){ return el % 2 === 1});
+  var even = numbers.filter(function(el){ return el % 2 === 0});
+
+  return odd.length < even.length ? (numbers.indexOf(odd[0]) + 1) : (numbers.indexOf(even[0]) + 1);
+}
