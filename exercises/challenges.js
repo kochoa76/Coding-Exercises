5d0ac183395a8ba5809e68dc375b913ca19d05a5
github username <github email address>
2018-12-28 10:06:12 -0600

    codewars exercise

--- exercises/challenges.js
+++ exercises/challenges.js
@@ -463,3 +463,26 @@ function duplicateCount(str){
 
 // duplicateCount('Indivisibility')
 // 1
+
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+//
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+
+function pigIt(str){
+  let newStr = str.split(' ')
+  let cutArray = newStr.map(word => word.slice(1))
+  let pigLatinArr = []
+  let selectedPLArr = []
+  for (var i=0; i < newStr.length; i++){
+
+		pigLatinArr.push(cutArray.map(cutWord => cutWord.concat(newStr[i].split('').shift())))
+
+  	selectedPLArr.push(pigLatinArr.map(arr => arr[i]))
+
+  }
+
+return selectedPLArr.map(arr => arr.pop().concat('ay')).join(' ')
+}
